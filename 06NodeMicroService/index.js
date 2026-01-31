17fb3f38bec66591b6542102956d6a91b744c322
Charan Raj <charan.raj@sap.com>
2026-01-31 23:10:21 +0530

    change

--- 06NodeMicroService/index.js
+++ 06NodeMicroService/index.js
@@ -10,3 +10,7 @@ app.use(express.static('webapp'));
 app.get('/', (req, res) => {
     res.send('Hello from the Node.js Microservice!');
 });
+
+app.get('/api/data', (req, res) => {
+    res.json({ message: 'This is some data from the microservice.' });
+});
